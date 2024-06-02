b850775ffbe844ff833b4c2eac8a6f69d522cc53
pseudo22 <hv885541@gmail.com>
2024-06-03 04:02:04 +0530

    added some things like cookie-parser, etc

--- server/app.js
+++ server/app.js
@@ -1,20 +1,26 @@
 import express from "express";
 import cors from 'cors'
-import test from './src/routes/test.js'
-import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 
 export const app = express()
-
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
     credentials : true
 }))
 
-app.use(bodyParser.json())
+app.use(express.json({limit : "20kB"}))
+app.use(express.urlencoded({extended : true}))
+app.use(cookieParser())
+app.use(express.static('public'))
 
-// routes for users
-app.use('/api' , test)
+//routes import
+import userRouter from './src/routes/user.routes.js'
+
+//routes declaration
+
+//user routes
+app.use('/api/users' , userRouter)
 
 
 
--- server/index.js
+++ server/index.js
@@ -11,9 +11,7 @@ dotenv.config({
 
 import {app} from './app.js';
 const PORT = process.env.PORT || 3000;
-app.get('/' , (req,res) => {
-  res.send('hello')
-})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
