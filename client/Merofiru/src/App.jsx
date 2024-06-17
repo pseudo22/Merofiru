79ad61726f46710a732ec792894e66310f11df01
pseudo22 <hv885541@gmail.com>
2024-06-18 03:51:39 +0530

    test

--- client/Merofiru/src/App.jsx
+++ client/Merofiru/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Login from './components/Login'
+import Login from './components/Login/MainLogin'
 function App() {
 
   return (
     <>
-      <Login/>
+    <Login/>
     </>
   )
 }
