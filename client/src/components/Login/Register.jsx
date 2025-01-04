9fa671d14b724cc3b4370b204fe8d4a3a8e02eaa
pseudo22 <hv885541@gmail.com>
2025-01-04 13:25:43 +0530

    validations

--- client/src/components/Login/Register.jsx
+++ client/src/components/Login/Register.jsx
@@ -1,7 +1,10 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { db } from '../../assets/firebaseConfig';
+import { collection, where, getDocs, query } from "firebase/firestore"
+
 
 function Register() {
 
@@ -21,12 +24,34 @@ function Register() {
       setError('not a valid email')
       return
     }
+    if(!EmailExists(email)){
+      setError('email already exists')
+      return
+    }
+
     if (password.length < 6) {
       setError('password is length less than 6 characters')
       return
     }
     setError('')
     navigate('/register/intro', { state: { email, password, isAccessible: true } })
+  }
+
+  async function EmailExists(email) {
+    if (email) {
+      try {
+        const userCollection = collection(db, 'users')
+
+        const q = query(userCollection, where('email', '==', email.trim()))
+
+        const querySnap = await getDocs(q)
+        return querySnap.empty
+      } catch (error) {
+        return false
+      }
+    } else {
+      return false
+    }
   }
 
   const isValidEmail = (email) => {
@@ -42,47 +67,47 @@ function Register() {
 
       <div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mx-2 items-center gap-y-4 p-4 flex flex-col rounded-lg bg-[#CCD0CF] shadow-lg md:w-96 w-full h-auto">
-        {error && <p className="text-red-500">{error}</p>}
+          {error && <p className="text-red-500">{error}</p>}
 
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="text-black p-2 rounded-lg outline outline-1 hover:outline-slate-700 outline-slate-500 w-full placeholder-gray-600"
-          placeholder="email"
-          required
-        />
-        <div className=" w-full">
           <input
-            type={showPassword ? "text" : "password"}
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="text-black p-2 rounded-lg outline outline-1 hover:outline-slate-700 outline-slate-500 w-full placeholder-gray-600"
-            placeholder="password?"
+            placeholder="email"
             required
           />
-          <span
-            className="absolute top-[40%] right-[5%] text-gray-600 cursor-pointer"
-            onClick={() => setShowPassword(!showPassword)}
+          <div className=" w-full">
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="text-black p-2 rounded-lg outline outline-1 hover:outline-slate-700 outline-slate-500 w-full placeholder-gray-600"
+              placeholder="password?"
+              required
+            />
+            <span
+              className="absolute top-[40%] right-[5%] text-gray-600 cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+            </span>
+          </div>
+          <button
+            onClick={handleContinue}
+            className={`text-white py-2 px-4 rounded-lg w-full ${email.length > 0 && password.length > 0 ? 'cursor-pointer bg-[#5cc6abeb]' : 'cursor-not-allowed bg-[#4087762e]'} mt-4 transition-all`}
+            disabled={!email.length > 0 && !password.length > 0}
           >
-            <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
-          </span>
-        </div>
-        <button
-          onClick={handleContinue}
-          className={`text-white py-2 px-4 rounded-lg w-full ${email.length > 0 && password.length > 0 ? 'cursor-pointer bg-[#5cc6abeb]' : 'cursor-not-allowed bg-[#4087762e]'} mt-4 transition-all`}
-          disabled={!email.length > 0 && !password.length > 0}
-        >
-          continue
-        </button>
+            continue
+          </button>
         </div>
       </div>
 
       <div
-            className="bottom-right m-1 absolute bottom-0 right-0 bg-[#5cc6abeb] w-16 h-16 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-36 lg:h-36 transform -rotate-90 flex items-center justify-center p-4 z-50 text-white text-sm sm:text-lg md:text-2xl lg:text-3xl rounded-lg shadow-lg"
-        >
+        className="bottom-right m-1 absolute bottom-0 right-0 bg-[#5cc6abeb] w-16 h-16 sm:w-24 sm:h-24 md:w-32 md:h-32 lg:w-36 lg:h-36 transform -rotate-90 flex items-center justify-center p-4 z-50 text-white text-sm sm:text-lg md:text-2xl lg:text-3xl rounded-lg shadow-lg"
+      >
         hello stranger
       </div>
     </>
